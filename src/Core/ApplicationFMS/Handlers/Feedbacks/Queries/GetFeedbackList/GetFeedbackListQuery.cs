5c5e053b71624e40a2b91bd451ac4a99bfd9b612
cemuluoglakci <cem.uluoglakci@metu.edu.tr>
2022-04-16 22:04:29 +0300

    Feedback classes initialized

---
+++ src/Core/ApplicationFMS/Handlers/Feedbacks/Queries/GetFeedbackList/GetFeedbackListQuery.cs
@@ -0,0 +1,32 @@
+﻿using ApplicationFMS.Models;
+using MediatR;
+using System;
+
+namespace ApplicationFMS.Handlers.Feedbacks.Queries.GetFeedbackList
+{
+    public class GetFeedbackListQuery : IRequest<BaseResponse<FeedbackListVm>>
+    {
+        public int ObjectsPerPage { get; set; } = 10;
+        public int PageNumber { get; set; } = 1;
+        public string SortColumn { get; set; } = "Id";
+        public bool IsAscending { get; set; } = true;
+        public string? TitleQuery { get; set; } = string.Empty;
+        public string? TextQuery { get; set; } = string.Empty;
+        public DateTime? CreatedAtBefore { get; set; }
+        public DateTime? CreatedAtAfter { get; set; }
+
+        public int? SectorId { get; set; }
+        public int? CompanyId { get; set; }
+        public int? ProductId { get; set; }
+        public int? TypeId { get; set; }
+        public int? SubTypeId { get; set; }
+
+        //Only for admins
+        public bool IsAnonym { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsChecked { get; set; }
+        public bool IsReplied { get; set; }
+        public bool IsSolved { get; set; }
+
+    }
+}
