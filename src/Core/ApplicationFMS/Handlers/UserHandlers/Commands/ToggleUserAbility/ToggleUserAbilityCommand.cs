8977b9595cd823117c228fd5bb470b633c7eea01
cemuluoglakci <cem.uluoglakci@metu.edu.tr>
2022-04-16 03:00:11 +0300

    ToggleUserAbility endpoint added

---
+++ src/Core/ApplicationFMS/Handlers/UserHandlers/Commands/ToggleUserAbility/ToggleUserAbilityCommand.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationFMS.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationFMS.Handlers.UserHandlers.Commands.ToggleUserAbility
+{
+    public class ToggleUserAbilityCommand : IRequest<BaseResponse<int>>
+    {
+        public int Id { get; set; }
+    }
+}
