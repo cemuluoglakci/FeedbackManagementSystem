b121d04287396209bd1d5f0c2b083a29f32a4bf7
cemuluoglakci <cem.uluoglakci@metu.edu.tr>
2022-04-16 22:04:29 +0300

    Feedback classes initialized

---
+++ src/Core/ApplicationFMS/Handlers/Feedbacks/Queries/GetFeedbackList/FeedbackListVm.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationFMS.Handlers.UserHandlers.Queries.GetUserList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationFMS.Handlers.Feedbacks.Queries.GetFeedbackList
+{
+    public class FeedbackListVm
+    {
+        public int TotalCount { get; set; }
+        public int FilteredCount { get; set; }
+        public int ObjectsPerPage { get; set; }
+        public int PageNumber { get; set; }
+        public IList<UserDTO> FeedbackList { get; set; }
+    }
+}
