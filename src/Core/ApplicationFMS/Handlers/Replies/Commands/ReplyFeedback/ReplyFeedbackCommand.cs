330604cd949cbcdd6353de6468480e877ea10888
cemuluoglakci <cem.uluoglakci@metu.edu.tr>
2022-04-18 13:40:37 +0300

    Post reply to a feedback

---
+++ src/Core/ApplicationFMS/Handlers/Replies/Commands/ReplyFeedback/ReplyFeedbackCommand.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationFMS.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationFMS.Handlers.Replies.Commands.ReplyFeedback
+{
+    public class ReplyFeedbackCommand : IRequest<BaseResponse<int>>
+    {
+        public int FeedbackId { get; set; }
+        public string Text { get; set; } = null!;
+    }
+}
---
+++ src/Presentation/FmsAPI/Controllers/ReplyController.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationFMS.Handlers.Feedbacks.Commands.ToggleArchived;
+using ApplicationFMS.Handlers.Replies.Commands.ReplyFeedback;
+using ApplicationFMS.Models;
+using FmsAPI.Helper;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FmsAPI.Controllers
+{
+    [Authorize]
+    public class ReplyController : BaseController
+    {
+
+        [HttpPost]
+        [Authorize("Company Employee", "Customer")]
+        public async Task<ActionResult<BaseResponse<int>>> PostReply ([FromBody] ReplyFeedbackCommand request)
+        {
+            return base.Ok(await Mediator.Send(request));
+        }
+
+    }
+}
