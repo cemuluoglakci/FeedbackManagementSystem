d137cc0f2fb26e27f6b7647493f368f948cbcfed
cemuluoglakci <cem.uluoglakci@metu.edu.tr>
2022-04-19 14:11:41 +0300

    Posting Comments

---
+++ src/Core/ApplicationFMS/Handlers/Comments/Commands/PostComment/PostCommentCommand.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationFMS.Models;
+using MediatR;
+
+namespace ApplicationFMS.Handlers.Comments.Commands.PostComment
+{
+    public class PostCommentCommand : IRequest<BaseResponse<int>>
+    {
+        public int FeedbackId { get; set; }
+        public int? ParentCommentId { get; set; }
+        public string Text { get; set; } = null!;
+        public bool IsAnonym { get; set; }
+
+    }
+}
