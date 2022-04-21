a2e072a973a9fde820e2785690732757018ce193
cemuluoglakci <cem.uluoglakci@metu.edu.tr>
2022-04-21 15:59:24 +0300

    System controller implemented

---
+++ src/Presentation/FmsAPI/Controllers/SystemController.cs
@@ -0,0 +1,27 @@
+﻿using ApplicationFMS.Handlers.System.Commands.SetOperationalMode;
+using ApplicationFMS.Handlers.System.Commands.SetTimeoutDuration;
+using ApplicationFMS.Helpers;
+using ApplicationFMS.Models;
+using FmsAPI.Helper;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FmsAPI.Controllers
+{
+    public class SystemController : BaseController
+    {
+        [HttpPut]
+        [Authorize(Constants.AdminRole)]
+        public async Task<ActionResult<BaseResponse<string>>> SetModeOfOperation([FromBody] SetOperationalModeCommand request)
+        {
+            return base.Ok(await Mediator.Send(request));
+        }
+
+        [HttpPut]
+        [Authorize(Constants.AdminRole)]
+        public async Task<ActionResult<BaseResponse<string>>> SetTimeoutDuration([FromBody] SetTimeoutDurationCommand request)
+        {
+            return base.Ok(await Mediator.Send(request));
+        }
+    }
+}
